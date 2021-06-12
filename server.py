bfd14ffcb5d37685d47b65ff37f4a44cd79db5ca
Cristian Caratti <ccristian.caratti.xx@gmail.com>
2021-06-12 12:14:40 +0100

    working with jinja and first simple form

--- server.py
+++ server.py
@@ -1,25 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from latin import latinizer
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/') 
 def index():
-    return "<p>Welcome! Go to /puppy_latin/name to see your name in puppy latin!</p>"
+    return render_template('index.html')
 
-@app.route("/info")
-def info():
-    return "<p>lorem ipsum</p>"
+@app.route('/signup') 
+def signup():
+    return render_template('signup.html')
 
-# @app. route( '/user/<name>')
+@app.route('/thankyou') 
+def thankyou():
+    first = request.args.get('firstname')
+    last = request.args.get('lastname')
+    fullname = first + ' ' + last
+    return render_template('thankyou.html',fullname=fullname)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404 
+
+@app.route("/playground.html")
+def playground():
+    name = 'Cristian'
+    api = { 'age':'42','language':'eng','height':'172'}
+    a_list = [1,2,3,4,5]
+    user_logged_in = True
+    return render_template('playground.html',your_name=name,api=api,a_list=a_list,user_logged_in=user_logged_in)
+
+@app.route( '/heroes/<name>')
+def user_page(name):
+    # return f'<h1>Hi, User: {name.upper()}!</h1>'
+    return render_template('heroes.html',name=name)
+
+# @app.route("/info")
+# def info():
+#     return "<p>lorem ipsum</p>"
+
+# @app. route( '/puppy_latin/<name>')
 # def other_page(name):
-#     return f'<h1>Hi, User: {name.upper()}!</h1>'
-
-@app. route( '/puppy_latin/<name>')
-def other_page(name):
-# Later we will see how to use
-# this parameter with templates!
-    return latinizer(name)
+#     return latinizer(name)
 
 if __name__ == '__main__':
     app.run()  
